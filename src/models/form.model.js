ff4df203255e44a876879e92091238ecc76df9d6
Yannick Marcon <yannick.marcon@obiba.org>
2021-11-15 15:10:09 +0100

    schema added to form model

--- src/models/form.model.js
+++ src/models/form.model.js
@@ -6,14 +6,17 @@ module.exports = function (app) {
   const modelName = 'form';
   const mongooseClient = app.get('mongooseClient');
   const { Schema } = mongooseClient;
+  const FormSchema = new Schema({
+    items: { type: [Object] },  // the form schema definition per items
+    i18n: { type: Object },     // the translation keys, by language
+    data: { type: Object }      // item options, data initialization
+  });
   const schema = new Schema({
     name: { type: String, required: true },
     description: { type: String, required: false },
     createdBy: { type: mongooseClient.Schema.Types.ObjectId, ref: 'user', required: true },
     study: { type: Schema.Types.ObjectId, ref: 'study' },
-    items: { type: [Object] },  // the form schema definition per items
-    i18n: { type: Object },     // the translation keys, by language
-    data: { type: Object }      // item options, data initialization
+    schema: { type: [FormSchema], default: {}}
   }, {
     timestamps: true
   });
