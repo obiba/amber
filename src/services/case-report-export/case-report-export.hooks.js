7fddd105265ee01e9d377a75ed2654eea9dfecea
Yannick Marcon <yannick.marcon@obiba.org>
2022-01-10 13:51:30 +0100

    authz access to case-report-form, crfs, case-report and case-report-export services

--- src/services/case-report-export/case-report-export.hooks.js
+++ src/services/case-report-export/case-report-export.hooks.js
@@ -1,9 +1,18 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authorize } = require('feathers-casl').hooks;
+const { defineAbilitiesFor } = require('./case-report-export.abilities');
+
+const makeAbilities = require('../../hooks/make-abilities');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
+    all: [
+      authenticate('jwt'),
+      makeAbilities(defineAbilitiesFor)
+    ],
+    find: [
+      authorize()
+    ],
     get: [],
     create: [],
     update: [],
--- src/services/case-report-form/case-report-form.hooks.js
+++ src/services/case-report-form/case-report-form.hooks.js
@@ -1,20 +1,41 @@
 const { authenticate } = require('@feathersjs/authentication').hooks;
+const { authorize } = require('feathers-casl').hooks;
+const { defineAbilitiesFor } = require('./case-report-form.abilities');
 
+const makeAbilities = require('../../hooks/make-abilities');
 const caseReportFormCreate = require('../../hooks/case-report-form-create');
 
 module.exports = {
   before: {
-    all: [ authenticate('jwt') ],
-    find: [],
-    get: [],
-    create: [caseReportFormCreate()],
-    update: [],
-    patch: [],
-    remove: []
+    all: [
+      authenticate('jwt'),
+      makeAbilities(defineAbilitiesFor)
+    ],
+    find: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
+    get: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
+    create: [
+      caseReportFormCreate(),
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
+    update: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
+    patch: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
+    remove: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ]
   },
 
   after: {
-    all: [],
+    all: [
+      authorize({ adapter: 'feathers-mongoose' })
+    ],
     find: [],
     get: [],
     create: [],
