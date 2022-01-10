ebc06a60ac14a54784e5e6df97476e8004e109f0
Yannick Marcon <yannick.marcon@obiba.org>
2022-01-10 17:38:33 +0100

    variables added to export in JSON

--- src/services/case-report-export/case-report-export.service.js
+++ src/services/case-report-export/case-report-export.service.js
@@ -9,19 +9,7 @@ var archiver = require('archiver');
 const { GeneralError } = require('@feathersjs/errors');
 
 const toCSV = (data, fields) => {
-  // _id is first column
-  const sortedFields = fields.sort((a, b) => {
-    if (a === '_id' || a < b) {
-      return -1;
-    }
-    if (b === '_id' || a > b) {
-      return 1;
-    }
-  
-    // names must be equal
-    return 0;
-  });
-  return parse(data, { fields: sortedFields });
+  return parse(data, { fields: fields });
 };
 
 const mkTmpDir = () => {
@@ -38,17 +26,108 @@ const doCsvResponse = (res) => {
   res.end(csv);
 };
 
+const makeAttributes = (key, i18n) => {
+  return Object.keys(i18n).map(locale => { 
+    return {
+      value: i18n[locale][key] ? i18n[locale][key] : key,
+      locale: locale
+    };
+  });
+};
+
+const makeVariables = (item, options) => {
+  const prefix = options.prefix ? options.prefix + '.' : '';
+  if (item.items) {
+    const variables = [];
+    item.items.forEach(it => {
+      const vars = makeVariables(it, {
+        entityType: options.entityType,
+        i18n: options.i18n,
+        prefix: item.name
+      });
+      if (Array.isArray(vars)) {
+        vars.forEach(v => variables.push(v));
+      } else {
+        variables.push(vars);
+      }
+    });
+    return variables;
+  } else {
+    const variable = {
+      name: prefix + item.name,
+      valueType: 'text',
+      entityType: options.entityType
+    };
+    // TODO get valueType from schema
+    if (item.type === 'toggle') {
+      variable.valueType = 'boolean';
+    } else if (['number', 'slider', 'rating'].includes(item.type)) {
+      variable.valueType = 'integer';
+    } // TODO date etc...
+    if (item.multiple) {
+      variable.isRepeatable = true;
+    }
+    variable.attributes = [];
+    ['label', 'description'].forEach(key => {
+      if (item[key]) {
+        makeAttributes(item[key], options.i18n).forEach(attr => 
+          variable.attributes.push({
+            name: key,
+            value: attr.value,
+            locale: attr.locale
+          }));
+      }
+    });
+    if (item.options) {
+      variable.categories = [];
+      item.options.forEach(opt => {
+        const category = {
+          name: opt.value
+        };
+        if (opt.label) {
+          category.attributes = makeAttributes(opt.label, options.i18n).map(attr => { 
+            return {
+              name: 'label',
+              value: attr.value,
+              locale: attr.locale
+            };
+          });
+        }
+        variable.categories.push(category);
+      });
+    }
+    return variable;
+  }
+};
+
 const doZipResponse = (res) => {
   const tmpDir = mkTmpDir();
   // write data
   for (const key in res.data) {
-    const csv = toCSV(res.data[key].data, res.data[key].fields);
     const tableName = res.data[key].formRevision.name + '-' + res.data[key].formRevision.revision;
     if (!fs.existsSync(path.join(tmpDir, tableName))) {
       fs.mkdirSync(path.join(tmpDir, tableName));
     }
-    fs.writeFileSync(path.join(tmpDir, tableName, 'data.csv'), csv); 
-    // TODO write variables
+    // write data
+    const csv = toCSV(res.data[key].data, res.data[key].fields);
+    fs.writeFileSync(path.join(tmpDir, tableName, 'data.csv'), csv);
+    // write variables
+    const schema = res.data[key].formRevision.schema;
+    const table = {
+      table: tableName,
+      entityType: 'Participant',
+      datasourceName: '',
+      variables: []
+    };
+    schema.items.forEach(item => {
+      const variables = makeVariables(item, { entityType: table.entityType, i18n: schema.i18n });
+      if (Array.isArray(variables)) {
+        variables.forEach(variable => table.variables.push(variable));
+      } else {
+        table.variables.push(variables);
+      }
+    });
+    fs.writeFileSync(path.join(tmpDir, tableName, 'table.json'), JSON.stringify(table));
   }
   const archive = archiver('zip');
   archive.on('error', (err) => {
