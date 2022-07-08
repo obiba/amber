507770d979d36e8e335c7a4db6845d87c31ce1ab
Yannick Marcon <yannick.marcon@obiba.org>
2022-07-08 11:22:24 +0200

    fix in records download

--- src/services/case-report-export/case-report-export.class.js
+++ src/services/case-report-export/case-report-export.class.js
@@ -63,9 +63,9 @@ exports.CaseReportExport = class CaseReportExport {
   }
 
   flattenByItems (items, data, path) {
-    const rval = {
+    const rval = data ? {
       _id: data._id
-    };
+    } : {};
     if (data) {
       items.forEach(item => {
         if (item.items) {
