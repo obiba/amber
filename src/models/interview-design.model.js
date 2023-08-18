5abe52407d2cfbb4c7fa83d623f84fdf62333ba1
Yannick Marcon <yannick.marcon@obiba.org>
2023-08-18 08:55:31 +0200

    fix: interview design repeat policy field removed

--- src/models/interview-design.model.js
+++ src/models/interview-design.model.js
@@ -31,7 +31,6 @@ module.exports = function (app) {
     steps: [{ type: stepSchema }],
     i18n: { type: Object, default: {} }, // the translation keys, by language
     state: { type: String, enum: ['active', 'paused'], default: 'paused' },
-    repeatPolicy: { type: String, enum: ['single_reject', 'single_update', 'multiple'], default: 'single_reject' },
     permissions: { type: permissionsSchema }
   }, {
     timestamps: true
