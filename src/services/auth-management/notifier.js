7776a5f7f5eeab6588bb21243518eb6cf4945015
Yannick Marcon <yannick.marcon@obiba.org>
2023-08-18 18:34:42 +0200

    feat: refactor notifications with a reusable mail builder class

--- src/services/auth-management/notifier.js
+++ src/services/auth-management/notifier.js
@@ -1,13 +1,12 @@
-const format = require('string-template');
-const fs = require('fs');
 const logger = require('../../logger');
+const { MailBuilder } = require('../../utils/mail');
 
 module.exports = function (app) {
 
   function getLink(type, user, hash) {
     let baseUrl = (process.env.AMBER_STUDIO_URL || app.get('amber_studio_url'));
-    // either registered itself from collect app or user was added as an interviewer/guest
-    if (user.clientId === 'amber_collect' || (!user.clientId && ['interviewer', 'guest'].includes(user.role))) {
+    // or registered itself from another app
+    if (user.clientId === 'amber_collect') {
       baseUrl = (process.env.AMBER_COLLECT_URL || app.get('amber_collect_url'));
     }
     if (user.clientId === 'amber_visit') {
@@ -18,129 +17,25 @@ module.exports = function (app) {
     return url;
   }
 
-  function sendEmail(email) {
-    email.headers = {
-      'x-amber': `${process.env.APP_NAME ? process.env.APP_NAME : '?'}`
-    };
-    if (!Array.isArray(email.to)) {
-      email.to = [ email.to ];
-    }
-    return app
-      .service('email')
-      .create(email)
-      .then(function (result) {
-        logger.debug('Sent email', result);
-      })
-      .catch((err) => {
-        logger.error('Error sending email', err);
-      });
-  }
-  const FROM_EMAIL = process.env.FROM_EMAIL ? process.env.FROM_EMAIL : app.get('from_email');
-
   return {
     service: 'user',
     notifier: function (type, user, data) {
-      logger.debug('type', type);
-      const emailTemplates = app.get('email_templates');
-      if (emailTemplates[type]) {
-        const subject = emailTemplates[type][user.language] ? emailTemplates[type][user.language].subject : emailTemplates[type].en.subject;
-        const file = emailTemplates[type][user.language] ? emailTemplates[type][user.language].file : emailTemplates[type].en.file;
-        let html;
-        // read template from file if it exists
-        if (file) {
-          try {
-            html = fs.readFileSync(file, 'utf8');
-          } catch (err) {
-            logger.error(`Unable to read email template: ${file}`, err);
-            html = undefined;
-          }
-        }
-        // fallback to html string from config
-        if (!html) {
-          html = emailTemplates[type][user.language] ? emailTemplates[type][user.language].html : emailTemplates[type].en.html;
-        }
-  
-        const context = {
-          email: user.email,
-          firstname: user.firstname,
-          lastname: user.lastname,
-          language: user.language,
-          role: user.role,
-          app_name: (process.env.APP_NAME || 'Amber'),
-          amber_studio_url: (process.env.AMBER_STUDIO_URL || app.get('amber_studio_url')),
-          amber_collect_url: (process.env.AMBER_COLLECT_URL || app.get('amber_collect_url')),
-          amber_visit_url: (process.env.AMBER_VISIT_URL || app.get('amber_visit_url'))
-        };
-
-        let email;
-        switch (type) {
-        case 'notifySignup':
-          // notifying user about a self-registration
-          logger.debug('data', data);
-          context.newUserEmail = data.email;
-          logger.debug('Email context', context);
-          email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        case 'resendVerifySignup':
-          //sending the user the verification email
-          context.tokenLink = getLink('verify', user, user.verifyToken);
-          logger.debug('Email context', context);
-          email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        case 'verifySignup':
-          // confirming verification
-          logger.debug('Email context', context);
-          email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        case 'sendResetPwd':
-          logger.debug('user', user);
-          context.tokenLink = getLink('reset-password', user, user.resetToken);
-          logger.debug('Email context', context);
-          email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        case 'resetPwd':
-          logger.debug('Email context', context);
-          email = email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        case 'passwordChange':
-          logger.debug('Email context', context);
-          email = {
-            from: FROM_EMAIL,
-            to: user.email,
-            subject: format(subject, context),
-            html: format(html, context),
-          };
-          return sendEmail(email);
-        default:
-          break;
-        }
-      } else {
-        logger.warn(`Email template is not defined for type: ${type}`);
+      const builder = new MailBuilder(app);
+      switch (type) {
+      case 'notifySignup': // notifying user about a self-registration
+        logger.debug('data', data);
+        return builder.sendEmail(type, user, { newUserEmail: data.email });
+      case 'resendVerifySignup': //sending the user the verification email link
+        return builder.sendEmail(type, user, { tokenLink: getLink('verify', user, user.verifyToken) });
+      case 'sendResetPwd': // sending the user the reset password link
+        return builder.sendEmail(type, user, { tokenLink: getLink('reset-password', user, user.resetToken) });
+      case 'verifySignup': // confirming verification
+      case 'resetPwd':
+      case 'passwordChange':
+        return builder.sendEmail(type, user);
+      default:
+        logger.warn(`Unknown user email type: ${type}`);
+        break;
       }
     },
   };
---
+++ src/utils/mail.js
@@ -0,0 +1,82 @@
+const format = require('string-template');
+const fs = require('fs');
+const logger = require('../logger');
+
+exports.MailBuilder = class MailBuilder {
+  constructor(app) {
+    this.app = app;
+  }
+
+  /**
+   * Make an email from string or file template of the provided type.
+   * @param {string} type The type of template
+   * @param {Object} user The recipient
+   * @param {Object} context The data that can be injected in the email subject/body
+   * @returns 
+   */
+  buildEmail(type, user, context) {
+    logger.debug('type', type);
+    const emailTemplates = this.app.get('email_templates');
+    if (emailTemplates[type]) {
+      const FROM_EMAIL = process.env.FROM_EMAIL ? process.env.FROM_EMAIL : this.app.get('from_email');
+      const subject = emailTemplates[type][user.language] ? emailTemplates[type][user.language].subject : emailTemplates[type].en.subject;
+      const file = emailTemplates[type][user.language] ? emailTemplates[type][user.language].file : emailTemplates[type].en.file;
+      let html;
+      // read template from file if it exists
+      if (file) {
+        try {
+          html = fs.readFileSync(file, 'utf8');
+        } catch (err) {
+          logger.error(`Unable to read email template: ${file}`, err);
+          html = undefined;
+        }
+      }
+      // fallback to html string from config
+      if (!html) {
+        html = emailTemplates[type][user.language] ? emailTemplates[type][user.language].html : emailTemplates[type].en.html;
+      }
+
+      const ctx = {
+        ...context,
+        email: user.email,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        language: user.language,
+        role: user.role,
+        app_name: (process.env.APP_NAME || 'Amber'),
+        amber_studio_url: (process.env.AMBER_STUDIO_URL || this.app.get('amber_studio_url')),
+        amber_collect_url: (process.env.AMBER_COLLECT_URL || this.app.get('amber_collect_url')),
+        amber_visit_url: (process.env.AMBER_VISIT_URL || this.app.get('amber_visit_url'))
+      };
+
+      logger.debug('Email context', ctx);
+
+      return {
+        from: FROM_EMAIL,
+        subject: format(subject, ctx),
+        html: format(html, ctx),
+      };
+    } else {
+      logger.error(`Email template is not defined for type: ${type}`);
+    }
+  }
+
+  sendEmail(type, user, context) {
+    const email = this.buildEmail(type, user, context);
+    if (!email) return;
+    
+    email.to = [ user.email ];
+    email.headers = {
+      'x-amber': `${process.env.APP_NAME ? process.env.APP_NAME : '?'}`
+    };
+    return this.app
+      .service('email')
+      .create(email)
+      .then(function (result) {
+        logger.debug('Sent email', result);
+      })
+      .catch((err) => {
+        logger.error('Error sending email', err);
+      });
+  }
+};
