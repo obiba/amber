7dd177139efcc7835962f696a577a7ace2ef6c34
Yannick Marcon <yannick.marcon@obiba.org>
2023-08-18 07:43:29 +0200

    fix: interviewer can read only campaigns for which s/he is an investigator

--- src/services/campaign/campaign.abilities.js
+++ src/services/campaign/campaign.abilities.js
@@ -21,7 +21,8 @@ const defineRulesFor = (user) => {
   } else if (user.role === 'manager') { 
     can('manage', 'campaign');
   } else if (user.role === 'interviewer') {
-    can('read', 'campaign');
+    // can read only campaigns for which s/he is an investigator
+    can('read', 'campaign', { investigators: user._id });
   }
 
   logger.debug('  rules: ' + JSON.stringify(rules, null, '  '));
