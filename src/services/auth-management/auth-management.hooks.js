6c6928bf07c25974c4278782798e22c6dd863315
Yannick Marcon <yannick.marcon@obiba.org>
2023-09-15 19:13:11 +0200

    feat: protect authManagement service output

--- src/services/auth-management/auth-management.hooks.js
+++ src/services/auth-management/auth-management.hooks.js
@@ -2,6 +2,8 @@ const userPasswordCheck = require('../../hooks/user-password-check');
 
 const userTotp2FaReset = require('../../hooks/user-totp2fa-reset');
 
+const { protect } = require('@feathersjs/authentication-local').hooks;
+
 module.exports = {
   before: {
     all: [],
@@ -14,7 +16,31 @@ module.exports = {
   },
 
   after: {
-    all: [],
+    all: [
+      protect(
+        'password',
+        'totp2faSecret',
+        'totp2faRequired',
+        'totp2faEnabled',
+        'updatedAt',
+        'createdAt',
+        'verifyToken',
+        'verifyShortToken',
+        'verifyExpires',
+        'resetToken',
+        'resetExpires',
+        'verifyChanges',
+        'firstname',
+        'lastname',
+        'clientId',
+        'isVerified',
+        'role',
+        'permissions',
+        'language',
+        '_id',
+        '__v'
+      )
+    ],
     find: [],
     get: [],
     create: [userTotp2FaReset()],
