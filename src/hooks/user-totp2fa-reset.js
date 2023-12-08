519c6860f4a953761db3ef43ee7bbcef758b4223
Yannick Marcon <yannick.marcon@obiba.org>
2023-12-08 12:14:14 +0100

    fix: make sure user id is defined when resetting 2fa token

--- src/hooks/user-totp2fa-reset.js
+++ src/hooks/user-totp2fa-reset.js
@@ -4,14 +4,16 @@
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
   return async context => {
-    if (context.result && context.result.totp2faSecret) {
-      // sanitize result
-      delete context.result.totp2faSecret;
-    }
-    if (context.data && context.data.action && context.data.action.startsWith('resetPwd')) {
-      // reset 2FA secret
-      context.result.totp2faEnabled = false;
-      context.app.service('user').patch(context.result._id, { totp2faSecret: null });
+    if (context.result) {
+      if (context.result.totp2faSecret) {
+        // sanitize result
+        delete context.result.totp2faSecret;
+      }
+      if (context.result._id && context.data && context.data.action && context.data.action.startsWith('resetPwd')) {
+        // reset 2FA secret
+        context.result.totp2faEnabled = false;
+        context.app.service('user').patch(context.result._id, { totp2faSecret: null });
+      }
     }
     return context;
   };
