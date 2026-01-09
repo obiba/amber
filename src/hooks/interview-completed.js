39c998f2d9df1192ab2bafae32402d3ef16f1f93
Yannick Marcon <yannick.marcon@obiba.org>
2026-01-09 15:07:16 +0100

    feat: send notification email on interview completion to campaign investigators

--- src/hooks/interview-completed.js
+++ src/hooks/interview-completed.js
@@ -1,5 +1,5 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const { MailBuilder } = require('../utils/mail');
+const logger = require('../logger');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = (options = {}) => {
@@ -9,6 +9,33 @@ module.exports = (options = {}) => {
       context.app.service('participant').patch(context.result.participant, {
         activated: false
       });
+      // get campaign administrators and send email notification about this participant's interview completion
+      const campaign = await context.app.service('campaign').get(context.result.campaign);
+      if (!campaign) {
+        return context;
+      }
+      const study = await context.app.service('study').get(campaign.study);
+      const interviewDesign = await context.app.service('interview-design').get(context.result.interviewDesign);
+      for (const investigator of campaign.investigators) {
+        try {
+          const builder = new MailBuilder(context.app);
+          // recipient
+          const user = await context.app.service('user').get(investigator);
+          // email context
+          const ctx = {
+            study: study.name,
+            interview: interviewDesign.name,
+            campaign: campaign.name,
+            study_id: study._id,
+            interview_id: interviewDesign._id,
+            campaign_id: campaign._id,
+            participant_code: context.result.code,
+          };
+          builder.sendEmail('interviewCompleted', user, ctx);
+        } catch (err) {
+          logger.error(`Error sending info participant has completed interview email to investigator ${investigator}: ${err.message}`);
+        }
+      }
     }
     return context;
   };
