04f349db3204fdd39308f1a4a375b059d8cf5d77
Yannick Marcon <yannick.marcon@obiba.org>
2026-06-10 16:04:34 +0200

    fix: validate array of objects

--- src/utils/validate-joi.js
+++ src/utils/validate-joi.js
@@ -18,6 +18,27 @@ const validateSchema = (schema, options = {}) => {
       return context;
     }
 
+    if (Array.isArray(data)) {
+      const values = [];
+      for (const item of data) {
+        const { error, value } = schema.validate(item, joiOptions);
+        if (error) {
+          const errors = {};
+          error.details.forEach(detail => {
+            const key = detail.path.join('.');
+            if (!errors[key]) {
+              errors[key] = [];
+            }
+            errors[key].push(detail.message);
+          });
+          throw new BadRequest('Validation failed', { errors });
+        }
+        values.push(value);
+      }
+      context.data = values;
+      return context;
+    }
+
     const { error, value } = schema.validate(data, joiOptions);
 
     if (error) {
